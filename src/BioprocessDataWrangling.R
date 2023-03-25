82e49c705252f94c67a1b7664013297f3b3575d4
Sean Nesdoly <srnesdoly@gmail.com>
2023-03-25 10:25:52 -0400

    Parse bioprocess data from a spreadsheet

--- src/BioprocessDataWrangling.R
+++ src/BioprocessDataWrangling.R
@@ -13,6 +13,7 @@
 
 # install.packages("tidyverse") # @TODO: uncomment & run if not yet installed!
 library(tidyverse)
+library(readxl)
 
 # Set working directory to location of 'BIEN675-BioprocessDataWrangling' git
 # repository. @TODO: Change filepath for your machine!
@@ -22,3 +23,52 @@ REPO_FILEPATH <- file.path(path.expand("~"),
                            fsep = .Platform$file.sep)
 setwd(REPO_FILEPATH)
 getwd()
+
+#-------------------------------------------------------------------------------
+# readxl: Read Excel spreadsheets (*.xls, *.xlsx)
+#-------------------------------------------------------------------------------
+# Define filepath to bioprocess dataset
+DATA_FILEPATH <- file.path("data",
+                           "lucullus_bioreactor_data.xlsx",
+                           fsep = .Platform$file.sep)
+
+# List sheets contained in spreadsheet
+readxl::excel_sheets(DATA_FILEPATH)
+
+# Peak at bioprocess data from spreadsheet 1
+View(readxl::read_xlsx(DATA_FILEPATH,
+                       sheet = 1,
+                       n_max = 100))
+
+# Extract bioprocess variable metadata
+bp_metadata <- read_xlsx(DATA_FILEPATH,
+                         sheet = 1,
+                         col_names = FALSE,
+                         n_max = 6) %>%
+                   t() %>%
+                   as_tibble() %>%
+                   rename(c("ProcessVariable"=V1, "Unit"=V2, "DeviceType"=V3,
+                            "Device"=V4, "Reactor"=V5, "ProcessName"=V6))
+
+# Parse bioprocess data from spreadsheet 1
+bp_data <- read_xlsx(DATA_FILEPATH,
+                     sheet = 1,
+                     skip = 6,
+                     col_names = bp_metadata$ProcessVariable,
+                     guess_max = as.integer(.Machine$integer.max / 100))
+
+# Explore bioprocess data!
+print(bp_data, n = 10)
+glimpse(bp_data)
+View(head(bp_data, n = 50))
+View(bp_data)
+
+# Parse capacitance data, a latent variable for biomass, from spreadsheet 2
+cap_data <- read_xlsx(DATA_FILEPATH,
+                      sheet = 2)
+
+# Explore capacitance data!
+print(cap_data, n = 10)
+glimpse(cap_data)
+View(head(cap_data, n = 50))
+View(cap_data)
